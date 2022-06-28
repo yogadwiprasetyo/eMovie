0b4305aee9581c628069dfa00364e31b573de230
Yoga Prasetyo <jobsforyoga@gmail.com>
2022-06-28 16:47:44 +0700

    refactoring code

--- core/src/main/java/com/yogaprasetyo/capstone/core/utils/Helper.kt
+++ core/src/main/java/com/yogaprasetyo/capstone/core/utils/Helper.kt
@@ -5,13 +5,12 @@ import android.widget.ImageView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.yogaprasetyo.capstone.core.R
 import com.yogaprasetyo.capstone.core.domain.model.Movie
 
-fun ImageView.loadImage(context: Context, source: Any?, isNotCircle: Boolean = true) {
-    val glide = Glide.with(context).load(source)
-    if (!isNotCircle) { glide.circleCrop() }
-    glide.error(R.drawable.ic_error_image).into(this)
+fun ImageView.loadImage(context: Context, source: Any?) {
+    Glide.with(context)
+        .load(source)
+        .into(this)
 }
 
 /**
@@ -27,7 +26,7 @@ fun getGenreByKey(key: Int): String? = genres[key]
 /**
  * Randomize the list and only take n items
  * */
-fun List<Movie>.preview(total: Int) = this.shuffled().take(total)
+fun List<Movie>.preview(total: Int) = this.take(total)
 
 /**
  * Calculate number of columns based width of items
@@ -41,12 +40,14 @@ fun calculateAutoColumns(rv: RecyclerView, columnWidth: Int): Int {
 /**
  * Class for compare the oldList and newList in ListAdapter
  * */
-class DiffCallback : DiffUtil.ItemCallback<Movie>() {
-    override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
-        return oldItem.movieId == newItem.movieId
-    }
+object Helper {
+    class DiffCallback : DiffUtil.ItemCallback<Movie>() {
+        override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem.movieId == newItem.movieId
+        }
 
-    override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
-        return oldItem == newItem
+        override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem == newItem
+        }
     }
 }
