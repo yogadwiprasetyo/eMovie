7eb330f229a06837bb94bc2bcfb4304994461f4e
Yoga Prasetyo <jobsforyoga@gmail.com>
2022-06-28 16:41:36 +0700

    remove and modified unimportant file

--- core/src/main/java/com/yogaprasetyo/capstone/core/data/NetworkBoundResource.kt
+++ core/src/main/java/com/yogaprasetyo/capstone/core/data/NetworkBoundResource.kt
@@ -28,7 +28,7 @@ abstract class NetworkBoundResource<ResultType, RequestType> {
                 }
                 is ApiResponse.Error -> {
                     onFetchFailed()
-                    emit(Resource.Error<ResultType>(apiResponse.errorMessage))
+                    emit(Resource.Error(apiResponse.errorMessage))
                 }
             }
         } else {
