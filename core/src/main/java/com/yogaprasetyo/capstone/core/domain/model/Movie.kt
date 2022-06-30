6a6d1e8c69deed02617079da1b389706c1ffe063
Yoga Prasetyo <jobsforyoga@gmail.com>
2022-06-30 18:16:52 +0700

    migration minSdk to 21

--- core/src/main/java/com/yogaprasetyo/capstone/core/domain/model/Movie.kt
+++ core/src/main/java/com/yogaprasetyo/capstone/core/domain/model/Movie.kt
@@ -1,7 +1,7 @@
 package com.yogaprasetyo.capstone.core.domain.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Movie(
