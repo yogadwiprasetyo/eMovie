19a87eb7cbb9b4a5dd181d79ab7099f933eb19e1
Yoga Prasetyo <jobsforyoga@gmail.com>
2022-06-30 18:27:00 +0700

    unit testing mock method for detail view model

---
+++ app/src/test/java/com/yogaprasetyo/capstone/emovie/detail/DetailViewModelTest.kt
@@ -0,0 +1,39 @@
+package com.yogaprasetyo.capstone.emovie.detail
+
+import com.yogaprasetyo.capstone.core.domain.usecase.MovieUseCase
+import com.yogaprasetyo.capstone.core.utils.testing.DataDummy.dummyMovie
+import com.yogaprasetyo.capstone.core.utils.testing.DataDummy.dummyState
+import com.yogaprasetyo.capstone.core.utils.testing.MainDispatcherRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.verifyNoMoreInteractions
+import org.mockito.junit.MockitoJUnitRunner
+
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class DetailViewModelTest {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @Mock
+    private lateinit var movieUseCase: MovieUseCase
+    private lateinit var detailViewModel: DetailViewModel
+
+    @Before
+    fun setup() {
+        detailViewModel = DetailViewModel(movieUseCase)
+    }
+
+    @Test
+    fun `when updateFavoriteMovie Called Should Invoke setFavoriteMovie Function`() = runTest {
+        detailViewModel.updateFavoriteMovie(dummyMovie, dummyState)
+        verify(movieUseCase).setFavoriteMovie(dummyMovie, dummyState)
+        verifyNoMoreInteractions(movieUseCase)
+    }
+}
